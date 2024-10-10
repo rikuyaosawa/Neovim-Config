1bdb29b89c7502862fd468dd214d052d1897511c
Osawa Rikuya <rikuyaosawa@gmail.com>
2024-10-10 16:46:39 +0900

    no search word lighlight

--- lua/rikuyaosawa/core/options.lua
+++ lua/rikuyaosawa/core/options.lua
@@ -28,7 +28,7 @@ opt.wrap = false -- disable line wrapping
 -- search settings
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
-opt.hls = true -- highlight searched words
+opt.hls = false -- highlight searched words
 opt.incsearch = true -- enable incremental search
 
 -- cursor line
