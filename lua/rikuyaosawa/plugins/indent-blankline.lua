15dd3e44cd8984634fcefaf74df33d6b045c2a1c
rikuyaosawa <rikuyaosawa@gmail.com>
2024-11-02 17:56:01 +0900

    downgrade the version to avoid bug

--- lua/rikuyaosawa/plugins/indent-blankline.lua
+++ lua/rikuyaosawa/plugins/indent-blankline.lua
@@ -1,8 +1,9 @@
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  main = "ibl",
-  opts = {
-    indent = { char = "┊" },
-  },
+	"lukas-reineke/indent-blankline.nvim",
+	tag = "v3.8.2",
+	event = { "BufReadPre", "BufNewFile" },
+	main = "ibl",
+	opts = {
+		indent = { char = "┊" },
+	},
 }
